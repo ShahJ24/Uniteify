8759a6bf428bdbdcec62efafedeee613d20e8689
Jeenal Shah <jeenal2416@example.com>
2021-11-11 16:14:53 -0500

    End of Section 8

--- API/Controllers/UsersControllers.cs
+++ API/Controllers/UsersControllers.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        //private readonly DataContext _context;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {  
-            _context = context;      
+            _mapper = mapper;
+            _userRepository = userRepository;
+            //_context = context;      
         }
 
 
@@ -35,12 +42,23 @@ namespace API.Controllers
 
         //Asynchronous Code
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {   
             // var users = _context.Users.ToList();
             // return users;
-            return await _context.Users.ToListAsync();
+            //return await _context.Users.ToListAsync();
+            
+            
+            /********* Another way of writing User Repository using a variable named users*********/
+            // var users = await _userRepository.GetUsersAsync();
+            // var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+            // return Ok(usersToReturn);
+
+            var users = await _userRepository.GetMembersAsync();
+            return Ok(users);
+            
+
+            //return Ok(await _userRepository.GetUsersAsync());
             
         } 
 
@@ -49,10 +67,16 @@ namespace API.Controllers
         //Second end point is to get the data for specific user - Async method
         //api/users/3    
         [Authorize]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {   
-            return await _context.Users.FindAsync(id);  
+            //return await _context.Users.FindAsync(id); 
+
+            // var user = await _userRepository.GetMemberAsync(username);
+            // return _mapper.Map<MemberDto>(user);
+
+            return await _userRepository.GetMemberAsync(username);
+            
         } 
     }
 }
--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -2,33 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Extensions;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDto
     {
         //Creating Id for the database and it will also work as the primary key
         public int Id { get; set; }
         //UserName - string for database
-        public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
+    
+        public int Age { get; set; }
         public string KnownAs { get; set; }
-        public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime LastActive { get; set; } = DateTime.Now;
+        public DateTime Created { get; set; } 
+        public DateTime LastActive { get; set; }
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<photo> Photos { get; set; }
+        public ICollection<PhotoDto> Photos { get; set; }
 
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
+        
     }
 }

0aa0055da33be4e6a13a2d324ea217f52addb118
Jeenal Shah <jeenal2416@example.com>
2021-11-17 14:13:59 -0500

    End of Section 10

--- API/Controllers/UsersControllers.cs
+++ API/Controllers/UsersControllers.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -26,57 +27,38 @@ namespace API.Controllers
             //_context = context;      
         }
 
-
-        /*
-        //first end point is to get the data for every user - Synchronous Code
-
-        [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
-        {   
-            var users = _context.Users.ToList();
-            return users;
-            // return _context.Users.ToList();
-            
-        } 
-        */
-
         //Asynchronous Code
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {   
-            // var users = _context.Users.ToList();
-            // return users;
-            //return await _context.Users.ToListAsync();
-            
-            
-            /********* Another way of writing User Repository using a variable named users*********/
-            // var users = await _userRepository.GetUsersAsync();
-            // var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
-            // return Ok(usersToReturn);
 
             var users = await _userRepository.GetMembersAsync();
             return Ok(users);
             
-
-            //return Ok(await _userRepository.GetUsersAsync());
-            
         } 
-
-
 
         //Second end point is to get the data for specific user - Async method
         //api/users/3    
         [Authorize]
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
-        {   
-            //return await _context.Users.FindAsync(id); 
-
-            // var user = await _userRepository.GetMemberAsync(username);
-            // return _mapper.Map<MemberDto>(user);
-
-            return await _userRepository.GetMemberAsync(username);
-            
+        {  
+            return await _userRepository.GetMemberAsync(username);           
         } 
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser (MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user!!");
+        }
     }
 }
